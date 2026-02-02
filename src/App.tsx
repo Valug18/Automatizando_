32e4f27e2c5b36d394a2f56e7fb454a4586e61b5
Gabriel Varela <110433213+Valug18@users.noreply.github.com>
2026-02-02 14:14:33 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -228,7 +228,7 @@ const App: React.FC = () => {
   ======================= */
   return (
     <div className="flex flex-col h-screen overflow-hidden bg-slate-100">
-      <div className="flex flex-1 overflow-hidden relative">
+      <div className="flex flex-col lg:flex-row flex-1 overflow-hidden relative">
         <Assistant />
 
         <div className="flex-1 flex flex-col min-w-0">
